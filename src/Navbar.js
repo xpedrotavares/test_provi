b7071c86c8424546e372d4f48119c4047ddf325f
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-12-07 16:33:21 -0300

    Navbar.js created

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import style from '../assets/style/style.css';
+// import UserPhoto from '../assets/images/Profile.jpg'
+
+const Navbar = () =>{
+    return (
+        <nav>
+        <img src=''/>
+        
+        </nav>
+     
+     
+    )
+}
+
+export default Navbar;
