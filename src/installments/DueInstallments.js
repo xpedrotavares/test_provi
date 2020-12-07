880a6e1e0d6f5407dfc92534964e518995fd7cf1
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-12-07 16:34:09 -0300

    DueInstallments created

---
+++ src/installments/DueInstallments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import style from '../assets/style/style.css';
+// import UserPhoto from '../assets/images/Profile.jpg'
+
+const DueInstallments = () =>{
+    return (
+     
+     
+    )
+}
+
+export default DueInstallments;
